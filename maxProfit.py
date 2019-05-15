cb02f3a3ceb6351728d354a4efcc11bad1683e72
Jaden Padua <46204970+forIoop@users.noreply.github.com>
2019-05-15 16:03:46 -0700

    Calculates the maximum profit of a list of integers
    Calculates the maximum profit of a list of integers if each index represented a day and each element represented a price

---
+++ maxProfit.py
@@ -0,0 +1,18 @@
+#Given a list of prices where i is a price of a given stock at that day,
+#find the maximum profit that you can get from the list of prices.
+
+prices  = [2,4,6,2,7,2,9,4,0,19,34,31]
+
+class Solution:
+    def maxProfit(prices) -> int:
+
+        max_profit = 0
+
+        for i in range(0,len(prices)):
+            for j in range(i+1, len(prices)):
+                min_price = prices[j] - prices[i]
+                if min_price> max_profit:
+                    max_profit = min_price
+        return max_profit
+
+    print(maxProfit(prices))
