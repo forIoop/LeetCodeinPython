cf6506b3fc8f8898cbbf446b4c59ec33f66c8560
Jaden Padua <46204970+forIoop@users.noreply.github.com>
2019-05-15 16:14:24 -0700

    Returns two indexes of an array that add to a target

--- twoSum.py
+++ twoSum.py
@@ -2,7 +2,7 @@
 #Given a sorted array of integers, return two numbers such that
 #they add up to a specific target, cannot use same element twice
 
-A = [-2,1,2,4,7,11]
+A = [-2,1,2,4,7,11,23]
 target = 13
 
 def two_sum_array_method(A,target):
