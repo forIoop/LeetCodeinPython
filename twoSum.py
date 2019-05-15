be618894ac9aed9bd2e3c8bb27f95610f3856d50
Jaden Padua <46204970+forIoop@users.noreply.github.com>
2019-05-15 15:13:52 -0700

    Add files via upload
    two sum in python solution brute force method

---
+++ twoSum.py
@@ -0,0 +1,12 @@
+#Given a sorted array of integers, return two numbers such that
+#they add up to a specific target, cannot use same element twice
+
+A = [-2,1,2,4,7,11]
+target = 13
+
+def two_sum_array_method(A,target):
+  for i in range(len(A)-1):
+    for j in range(i+1, len(A)):
+      if A[i] + A[j] == target:
+        print(A[i], A[j])
+        return True
