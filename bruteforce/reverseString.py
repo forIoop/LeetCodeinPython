1f91a2e1aad18e49c980319143e6d0856c96234f
Jaden Padua <46204970+forIoop@users.noreply.github.com>
2019-05-16 19:59:47 -0700

    function that reverses any string inputted by the user

---
+++ bruteforce/reverseString.py
@@ -0,0 +1,13 @@
+#write a function that reverses any string inputted by the user
+
+def reverse(string):
+
+    reversed_string = ""
+    for i in string:
+      reversed_string = i + reversed_string
+
+    print("Reversed String: " + reversed_string)
+
+string = input("Enter any string: ")
+print("Your entered string " + string)
+reverse(string)
