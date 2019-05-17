0b79d1f10a2e16e1ff709a4157f6a23e82813775
Jaden Padua <46204970+forIoop@users.noreply.github.com>
2019-05-17 00:19:46 -0700

    Determine whether an integer is a palindrome. Olog(n)

---
+++ isPalindrome.py
@@ -0,0 +1,26 @@
+#Determine whether an integer is a palindrome. 
+#An integer is a palindrome when it reads the same backward as forward.
+def isPalindrome(n):
+    num = n
+    d = 0
+    rev = 0
+
+    while n > 0:
+        d = n % 10
+        n = int(n/10)
+        rev = rev*10 + d
+
+    if num == rev:
+        return True
+
+    else:
+        return False
+
+
+x = int(input("enter a number: "))
+
+if isPalindrome(x):
+    print(x,"is a palindrome number!")
+
+else:
+    print(x,"is not a palindrome number!")
