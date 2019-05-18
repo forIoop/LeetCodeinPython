733817cb0dc4954b9b52129ef1ec2544aff1d8e0
Jaden Padua <46204970+forIoop@users.noreply.github.com>
2019-05-17 18:55:06 -0700

    #Determine whether an integer is a palindrome. O(n).

--- bruteforce/isPalindrome.py
+++ bruteforce/isPalindrome.py
@@ -1,4 +1,4 @@
-#Determine whether an integer is a palindrome. 
+#Determine whether an integer is a palindrome.  
 #An integer is a palindrome when it reads the same backward as forward.
 def isPalindrome(n):
     num = n
