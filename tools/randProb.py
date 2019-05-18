fb47b2b3755a218aa9998332a1c16119ed6a1967
Jaden Padua <46204970+forIoop@users.noreply.github.com>
2019-05-18 16:59:51 -0700

    tool for leetcode problem training

---
+++ tools/randProb.py
@@ -0,0 +1,6 @@
+import random
+
+problemSet1 = ['Two Sum','Reverse Integer','Palindrom Number','Roman to integer','Longest Common Prefix','Roman to Integer','Valid Parentheses','buy and sell stocks']
+
+reviewProblem1 =  random.choice(problemSet1)
+print("Randomly chosen problem from problem set 1: " + (reviewProblem1))
