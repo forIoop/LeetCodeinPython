bf27f3cdf781536c027af6b3106d273da8498933
Jaden Padua <46204970+forIoop@users.noreply.github.com>
2019-05-19 18:54:41 -0700

    added merge two sorted LL

--- tools/randProb.py
+++ tools/randProb.py
@@ -1,6 +1,7 @@
 import random
 
-problemSet1 = ['Two Sum','Reverse Integer','Palindrom Number','Roman to integer','Longest Common Prefix','Roman to Integer','Valid Parentheses','buy and sell stocks']
+problemSet1 = ['Two Sum','Reverse Integer','Palindrom Number','Roman to integer','Longest Common Prefix',
+              'Roman to Integer','Valid Parentheses','buy and sell stocks','merge two sorted lists']
 
 reviewProblem1 =  random.choice(problemSet1)
 print("Randomly chosen problem from problem set 1: " + (reviewProblem1))
