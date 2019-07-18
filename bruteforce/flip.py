e1eeac612968b85588fe35f1b74ba59c1909f23b
Jaden Padua <46204970+forIoop@users.noreply.github.com>
2019-07-18 14:34:13 -0700

    Create flip.py

---
+++ bruteforce/flip.py
@@ -0,0 +1,14 @@
+Write a function that returns 0 if the input is 1, and returns 1 if the input is 0.
+
+Examples
+flip(1) ➞ 0
+
+flip(0) ➞ 1
+
+
+def flip(y):
+	if y == 1:
+		return 0
+	elif y == 0:
+		return 1
+	
