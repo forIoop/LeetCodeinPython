95ef6be37dbf5631727b24f820f769affa6e119f
Jaden Padua <46204970+jadenpadua@users.noreply.github.com>
2019-09-13 14:42:50 -0700

    Create stringOperation.cpp

---
+++ C++/concepts/intro/stringOperation.cpp
@@ -0,0 +1,20 @@
+std::string operation(int num1, int num2) {
+	if(num1 + num2 == 24) {
+		return "added";
+	}
+	else if(num1 - num2 == 24) {
+		return "subtracted";
+	}
+	
+	else if( num1 * num2 == 24) {
+		return "multiplied";
+	}
+
+	else if(num1 / num2 == 24){
+		return "divided";
+	}
+	
+	else {
+		return "Invalid";
+	}
+}
