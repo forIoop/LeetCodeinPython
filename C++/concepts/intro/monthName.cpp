3f6b19bdb69d7c7f32c8a34d1bf8d471fd8941cc
Jaden Padua <46204970+jadenpadua@users.noreply.github.com>
2019-09-13 15:17:41 -0700

    Create monthName.cpp

---
+++ C++/concepts/intro/monthName.cpp
@@ -0,0 +1,4 @@
+std::string monthName(int num) {
+	std::string months[12] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October","November","December"};
+	return months[num-1];
+}
