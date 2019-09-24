32883ea770610fd62d1e797f7b6ccbe6d30761ab
Jaden Padua <46204970+jadenpadua@users.noreply.github.com>
2019-09-23 22:39:57 -0700

    Update maxSubArray.py

--- 75/arrays/maxSubArray.py
+++ 75/arrays/maxSubArray.py
@@ -1,8 +1,13 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+        #Set current and global to default 1st element
         max_current = max_global = A[0]
+        
         for i in range(1,len(nums)-1):
+            #current_max is the max between that element and previous current_max
             max_current = max(A[i],max_current + A[i])
+            # now we need to store and ultimate global max
             if max_current > max_global
                 max_global = max_current
         return max_global
+    # O(n) time | O(1) space
