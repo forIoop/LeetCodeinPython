9f4ae418b4348c3f5d1c04f6b4a46415dfd26783
Jaden Padua <46204970+jadenpadua@users.noreply.github.com>
2019-09-25 21:12:55 -0700

    Create climbingStairs_MEF.py

---
+++ 75/DynamicProgramming/climbingStairs_MEF.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def climbStairs(self, n): 
+        if n == 1:
+            return 1
+        a, b = 1, 2
+        for i in range(2,n):
+            tmp = b
+            b = a + b
+            a = tmp
+        return b
+    
+    #O(n) and O(1) 
